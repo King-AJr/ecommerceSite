eb7ee96eda688f4821c26e3d1b0010dddc2214c8
King-AJr <talk2king.aj@gmail.com>
2024-04-10 14:55:16 +0100

    restarting with a better UI design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from "./pages/HomePage";
 
 
 function App() {
   return (
-    <Header/>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </Router>
+    </>
   )
 }
 
